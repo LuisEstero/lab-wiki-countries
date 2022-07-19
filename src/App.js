9a8a00351e6f4585e58031127fa36a7df8c5f492
LuisEstero <luisestero@gmail.com>
2022-07-19 15:51:12 +0200

    Lab 21

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
-
-import "./App.css";
+import './App.css';
+import Navbar from './components/Navbar';
+import CountriesList from './components/CountriesList';
+import { Routes, Route } from 'react-router-dom';
+import CountryDetails from './components/CountryDetails';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+
+      <div style={{display: "flex"}}>
+        <CountriesList />
+
+        <Routes>
+          <Route path="/:alpha3Code" element={<CountryDetails />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 export default App;
